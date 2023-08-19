a37bb174c89f85c2fb791dbc48ece81565915381
Julius Kirsch <Julius@skirsch.de>
2023-08-19 22:56:19 +0200

    initially added language items for de-DE & en-US

---
+++ EasePass/Helper/LocalizationHelper.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.Windows.ApplicationModel.Resources;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EasePass.Helper
+{
+    internal class LocalizationHelper
+    {
+        public static string GetString(string key)
+        {
+            ResourceManager resourceManager = new ResourceManager();
+            return resourceManager.MainResourceMap.GetValue(key).ValueAsString;
+        }
+
+        public static void ChangeLanguage(string language)
+        {
+            Windows.Globalization.ApplicationLanguages.PrimaryLanguageOverride = language;
+        }
+
+
+        public IReadOnlyList<string> Languages => Windows.Globalization.ApplicationLanguages.Languages;
+    }
+}
