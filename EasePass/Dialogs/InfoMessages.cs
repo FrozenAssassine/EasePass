d40608a79dfe4b11d2bfc421118392af81aa36f5
Julius Kirsch <Julius@skirsch.de>
2023-08-20 08:20:42 +0200

    refactor: correctly named infomessage

--- EasePass/Dialogs/InfoMessages.cs
+++ EasePass/Dialogs/InfoMessages.cs
@@ -8,7 +8,7 @@ namespace EasePass.Dialogs
     {
         public static void EnteredWrongPassword(int attempts) => new InfoBar().Show("Wrong password", $"You entered the wrong password.\nPlease try again\n({attempts}/3)", InfoBarSeverity.Error);
         public static void TooManyPasswordAttempts() => new InfoBar().Show("Too many attempts", "You entered the password wrong three times.", InfoBarSeverity.Error);
-        public static void ExportDBWrongPassword() => new InfoBar().Show("Wrong password", "Could not import database, because you've entered the wrong password.", InfoBarSeverity.Error);
+        public static void ImportDBWrongPassword() => new InfoBar().Show("Wrong password", "Could not import database, because you've entered the wrong password.", InfoBarSeverity.Error);
         public static void ImportDBSuccess() => new InfoBar().Show("Imported successfully", "Imported data successfully", InfoBarSeverity.Success);
         public static void ExportDBSuccess() => new InfoBar().Show("Export successfully", "Exported data successfully", InfoBarSeverity.Success);
         public static void PasswordTooShort() => new InfoBar().Show("Password too short", "The password is too short", InfoBarSeverity.Error);
