e5b7af08c89cf112298cd3a82dea424ea960b6de
Julius Kirsch <Julius@skirsch.de>
2023-08-21 08:11:50 +0200

    refactor: fixed spelling in export db info message

--- EasePass/Dialogs/InfoMessages.cs
+++ EasePass/Dialogs/InfoMessages.cs
@@ -10,7 +10,7 @@ namespace EasePass.Dialogs
         public static void TooManyPasswordAttempts() => new InfoBar().Show("Too many attempts", "You entered the password wrong three times.", InfoBarSeverity.Error);
         public static void ExportDBWrongPassword() => new InfoBar().Show("Wrong password", "Could not import database, because you've entered the wrong password.", InfoBarSeverity.Error);
         public static void ImportDBSuccess() => new InfoBar().Show("Imported successfully", "Imported data successfully", InfoBarSeverity.Success);
-        public static void ExportDBSuccess() => new InfoBar().Show("Export successfully", "Exported data successfully", InfoBarSeverity.Success);
+        public static void ExportDBSuccess() => new InfoBar().Show("Exported successfully", "Exported data successfully", InfoBarSeverity.Success);
         public static void PasswordTooShort() => new InfoBar().Show("Password too short", "The password is too short", InfoBarSeverity.Error);
         public static void ChangePasswordWrong() => new InfoBar().Show("Wrong password", "The current password is incorrect", InfoBarSeverity.Error);
         public static void PasswordsDoNotMatch() => new InfoBar().Show("Passwords do not match", "", InfoBarSeverity.Error);
