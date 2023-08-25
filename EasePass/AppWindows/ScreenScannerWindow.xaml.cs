db878bd6880f6d54b25f964c9a4e8f187c1bf777
Julius Kirsch <Julius@skirsch.de>
2023-08-25 21:46:41 +0200

    fix for high memory usage even after closing #21

--- EasePass/AppWindows/ScreenScannerWindow.xaml.cs
+++ EasePass/AppWindows/ScreenScannerWindow.xaml.cs
@@ -1,24 +1,16 @@
 using EasePass.Extensions;
 using EasePass.Helper;
+using Microsoft.UI.Windowing;
 using Microsoft.UI.Xaml;
-using Microsoft.UI.Xaml.Controls;
-using Microsoft.UI.Xaml.Controls.Primitives;
-using Microsoft.UI.Xaml.Data;
-using Microsoft.UI.Xaml.Input;
-using Microsoft.UI.Xaml.Media;
-using Microsoft.UI.Xaml.Navigation;
 using System;
-using System.Collections.Generic;
+using System.Diagnostics;
 using System.Drawing;
 using System.Drawing.Imaging;
 using System.IO;
-using System.Linq;
 using System.Runtime.InteropServices;
 using System.Runtime.InteropServices.WindowsRuntime;
-using System.Timers;
-using Windows.Foundation;
-using Windows.Foundation.Collections;
 using Windows.Graphics.Imaging;
+using WindowsDisplayAPI;
 
 namespace EasePass.AppWindows
 {
@@ -26,7 +18,7 @@ namespace EasePass.AppWindows
     {
         public string Result { get; set; } = "";
 
-        private Timer timer;
+        private DispatcherTimer timer = new DispatcherTimer();
         private QRCodeScanner scanner;
 
         int width = 0;
@@ -39,23 +31,25 @@ namespace EasePass.AppWindows
             this.ExtendsContentIntoTitleBar = true;
             this.SetTitleBar(panel);
             this.AppWindow.Resize(new Windows.Graphics.SizeInt32(400, 70));
+            this.AppWindow.Closing += AppWindow_Closing;
+
             WindowHelper.MakeToolWindow(this);
         }
 
-        private void Window_Activated(object sender, WindowActivatedEventArgs args)
+
+        private void Grid_Loaded(object sender, RoutedEventArgs e)
         {
             // TODO init width and height with screen bounds
             width = 2000;
             height = 2000;
 
             scanner = new QRCodeScanner();
-            timer = new Timer();
-            timer.Interval = 1000;
-            timer.Elapsed += Timer_Elapsed;
+            timer.Interval = new TimeSpan(0,0,5);
+            timer.Tick += Timer_Tick; 
             timer.Start();
         }
 
-        private async void Timer_Elapsed(object sender, ElapsedEventArgs e)
+        private async void Timer_Tick(object sender, object e)
         {
             Bitmap bmp = new Bitmap(width, height);
             Graphics g = Graphics.FromImage(bmp);
@@ -68,7 +62,7 @@ namespace EasePass.AppWindows
                 BitmapDecoder decoder = await BitmapDecoder.CreateAsync(stream);
                 SoftwareBitmap softwareBitmap = await decoder.GetSoftwareBitmapAsync();
                 Result = scanner.Scan(softwareBitmap);
-                if(Result != "")
+                if (Result != "")
                 {
                     DispatcherQueue.TryEnqueue(async () =>
                     {
@@ -79,9 +73,9 @@ namespace EasePass.AppWindows
             }
         }
 
-        private void Close_Btn_Click(object sender, RoutedEventArgs e)
+        private void AppWindow_Closing(Microsoft.UI.Windowing.AppWindow sender, Microsoft.UI.Windowing.AppWindowClosingEventArgs args)
         {
-            this.Close();
+            timer.Stop();
         }
     }
 }
