ce2a7d71eed880d106c5ffb0d187cdf5ba1f0b3e
Julius Kirsch <Julius@skirsch.de>
2024-01-20 00:15:17 +0100

    feat: changelog display in aboutpage #75

--- EasePass/Dialogs/InfoMessages.cs
+++ EasePass/Dialogs/InfoMessages.cs
@@ -1,4 +1,5 @@
 ﻿using EasePass.Extensions;
+using EasePass.Views;
 using Microsoft.UI.Xaml.Controls;
 
 namespace EasePass.Dialogs
@@ -28,7 +29,11 @@ namespace EasePass.Dialogs
         public static void FileIsNotAnExtensions() => new InfoBar().Show("File is not a plugin", "The selected file is not an Ease Pass plugin!", InfoBarSeverity.Error, 10);
         public static void NewVersionInfo(string version)
         {
-            var btn = new HyperlinkButton { Content = "Changelog", NavigateUri = new System.Uri("https://github.com/FrozenAssassine/EasePass/blob/master/changelog.md") };
+            var btn = new Button { Content = "Changelog" };
+            btn.Click += delegate
+            {
+                App.m_frame.Navigate(typeof(AboutPage), 0);
+            };
 
             new InfoBar().Show("New version", "Welcome to Ease Pass version " + version + "\n", btn, InfoBarSeverity.Success);
         }
--- EasePass/Views/AboutPage.xaml.cs
+++ EasePass/Views/AboutPage.xaml.cs
@@ -1,7 +1,10 @@
 using EasePass.Helper;
 using Microsoft.UI.Xaml.Controls;
+using Microsoft.UI.Xaml.Documents;
 using Microsoft.UI.Xaml.Navigation;
 using System;
+using System.Collections.Generic;
+using System.IO;
 using Windows.ApplicationModel;
 
 namespace EasePass.Views
@@ -16,6 +19,8 @@ namespace EasePass.Views
         {
             this.InitializeComponent();
             App.m_window.ShowBackArrow = true;
+
+            SetChangelog();
         }
 
         private async void NavigateToLink_Click(Controls.SettingsControl sender)
@@ -31,5 +36,49 @@ namespace EasePass.Views
             base.OnNavigatedFrom(e);
             App.m_window.ShowBackArrow = false;
         }
+
+        protected override void OnNavigatedTo(NavigationEventArgs e)
+        {
+            //send any parameter opens the changelog
+            if (e.Parameter != null)
+            {
+                changelogExpander.IsExpanded = true;
+            }
+            base.OnNavigatedTo(e);
+        }
+
+        private void SetChangelog()
+        {
+            //Simple parser to make headlines bigger and add paragraphs
+            string filePath = Path.Combine(Package.Current.InstalledLocation.Path, "Assets", "changelog.txt");
+            var data = File.ReadAllLines(filePath);
+            List<Paragraph> paragraphs = new List<Paragraph> { new Paragraph() };
+            for (int i = 0; i < data.Length; i++)
+            {
+                string currentLine = data[i];
+                Run line = new Run();
+
+                //Headline:
+                if (currentLine.StartsWith("#"))
+                {
+                    currentLine = currentLine.Remove(0, 1);
+                    line.FontSize = 24;
+                }
+                //Paragraph
+                else if (currentLine.StartsWith("---"))
+                {
+                    currentLine = currentLine.Remove(0, 3);
+                    paragraphs.Add(new Paragraph());
+                }
+
+                line.Text = currentLine + "\n";
+                paragraphs[paragraphs.Count - 1].Inlines.Add(line);
+            }
+            foreach (var paragraph in paragraphs)
+            {
+                ChangelogDisplay.Blocks.Add(paragraph);
+            }
+        }
+
     }
 }
