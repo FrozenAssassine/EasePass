8c3f3ba2bd4414f8f514ece48b6b288808f03e0f
Finn Freitag <finn-freitag@web.de>
2024-04-21 17:59:07 +0200

    feat/fix: better print functionality

--- EasePass/Helper/PrinterHelper.cs
+++ EasePass/Helper/PrinterHelper.cs
@@ -7,6 +7,9 @@ using System.Drawing;
 using System.Drawing.Printing;
 using System.Text;
 using System.Threading.Tasks;
+using ZXing.QrCode;
+using ZXing;
+using ZXing.Windows.Compatibility;
 
 namespace EasePass.Helper
 {
@@ -38,6 +41,7 @@ namespace EasePass.Helper
                 PrinterHelper.items = items;
                 PrintDocument pd = new PrintDocument();
                 pd.PrinterSettings.PrinterName = SelectedPrinter;
+                pd.DefaultPageSettings.Landscape = false;
                 pd.PrintPage += Pd_PrintPage;
                 pd.EndPrint += Pd_EndPrint;
                 PageIndex = 0;
@@ -60,6 +64,7 @@ namespace EasePass.Helper
             float yOffset = Math.Max(CmToInch(TopSpacing) * dpiy, 0/*e.MarginBounds.Top*/);
             float bottomSpace = Math.Max(CmToInch(BottomSpacing) * dpiy, 0/*e.MarginBounds.Bottom*/);
             float leftSpace = Math.Max(CmToInch(LeftSpacing) * dpix, 0/*e.MarginBounds.Left*/);
+            float rightSpace = Math.Max(CmToInch(RightSpacing) * dpix, 0/*e.MarginBounds.Right*/);
             if (PageIndex == 0)
             {
                 SizeF size = e.Graphics.MeasureString("Ease Pass", HeadingFont);
@@ -70,6 +75,7 @@ namespace EasePass.Helper
             while (ItemIndex < items.Count)
             {
                 var item = BuildServiceString(items[ItemIndex]);
+                var qrCode = CreateQrCodeFrom2FA(items[ItemIndex]);
                 float textHeight = 0;
                 SizeF displayNameSize = e.Graphics.MeasureString(item.name, DisplayNameFont);
                 textHeight += displayNameSize.Height;
@@ -83,10 +89,13 @@ namespace EasePass.Helper
                     continue;
                 }
                 if (yOffset + textHeight + bottomSpace > height) break;
+                float yOffBack = yOffset;
                 e.Graphics.DrawString(item.name, DisplayNameFont, Brush, new PointF(leftSpace, yOffset));
                 yOffset += displayNameSize.Height;
                 yOffset += 0.1f * dpiy;
                 e.Graphics.DrawString(item.body, BodyFont, Brush, new PointF(leftSpace, yOffset));
+                if (qrCode.status)
+                    e.Graphics.DrawImage(qrCode.qrCode, new RectangleF(width - rightSpace - 200, yOffBack, 200, 200));
                 yOffset += bodySize.Height;
                 yOffset += 0.5f * dpiy; // 1/2 inch spacing between services
                 ItemIndex++;
@@ -117,9 +126,35 @@ namespace EasePass.Helper
             if (!string.IsNullOrEmpty(item.Notes))
             {
                 sb.AppendLine("Notes:".Localized("PrintDB_Notes/Text"));
-                sb.AppendLine(item.Notes);
+                string[] lines = item.Notes.Replace("\n","\n\n").Replace('\r','\n').Replace("\n\n","\n").Split('\n');
+                foreach (string line in lines)
+                    sb.AppendLine(line);
             }
             return (item.DisplayName, sb.ToString());
+        }
+
+        private static (bool status, Bitmap qrCode) CreateQrCodeFrom2FA(PasswordManagerItem item)
+        {
+            if (string.IsNullOrEmpty(item.Secret))
+                return (false, null);
+
+            string codeContent = TOTP.EncodeUrl(item.DisplayName, string.IsNullOrEmpty(item.Username) ? item.Email : item.Username, item.Secret, TOTP.StringToHashMode(item.Algorithm), Convert.ToInt32(item.Digits), Convert.ToInt32(item.Interval));
+
+            QrCodeEncodingOptions options = new QrCodeEncodingOptions()
+            {
+                DisableECI = true,
+                CharacterSet = "UTF-8",
+                Width = 300,
+                Height = 300
+            };
+
+            BarcodeWriter writer = new BarcodeWriter()
+            {
+                Format = BarcodeFormat.QR_CODE,
+                Options = options
+            };
+
+            return (true, writer.Write(codeContent));
         }
     }
 }
