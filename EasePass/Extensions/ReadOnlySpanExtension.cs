92a5ad72b575437d0f8ecb49ff9859d551a9dbcc
Philipp Eckert <Philippeckert06@gmail.com>
2024-12-19 21:00:58 +0100

    added Count Method for the ReadOnlySpan<char>

---
+++ EasePass/Extensions/ReadOnlySpanExtension.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace EasePass.Extensions
+{
+    internal static class ReadOnlySpanExtension
+    {
+        /// <summary>
+        /// Counts every occurence of the given <paramref name="c"/> in the <paramref name="span"/>
+        /// </summary>
+        /// <param name="span">The Span which chars will be checked</param>
+        /// <param name="c">The Character which should be counted</param>
+        /// <returns>Returns the amount of <paramref name="c"/> occurences in <paramref name="span"/></returns>
+        public static int Count(this ReadOnlySpan<byte> span, char c)
+        {
+            int count = 0;
+            int length = span.Length;
+            for (int i = 0; i < length; i++)
+            {
+                if (span[i] == c)
+                {
+                    count++;
+                }
+            }
+            return count;
+        }
+    }
+}
