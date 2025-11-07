c395c6e63e0fb99719f6acad4bda7473c44f843b
Julius Kirsch <Julius@skirsch.de>
2025-11-07 09:38:54 +0100

    feat: store window state + size properly

--- EasePass/MainWindow.xaml.cs
+++ EasePass/MainWindow.xaml.cs
@@ -14,122 +14,89 @@ The above copyright notice and this permission notice shall be included in all
 copies or substantial portions of the Software.
 */
 
+using EasePass.Core;
 using EasePass.Core.Database;
 using EasePass.Dialogs;
 using EasePass.Helper;
-using EasePass.Settings;
 using EasePass.Views;
 using Microsoft.UI.Dispatching;
-using Microsoft.UI.Windowing;
 using Microsoft.UI.Xaml;
 using Microsoft.UI.Xaml.Controls;
-using System;
 using System.IO;
 using Windows.ApplicationModel;
 
-namespace EasePass
+namespace EasePass;
+
+public sealed partial class MainWindow : Window
 {
-    public sealed partial class MainWindow : Window
+    public InactivityHelper inactivityHelper = new InactivityHelper();
+    public static StackPanel InfoMessagesPanel;
+    public Frame MainFrame => navigationFrame;
+    public bool ShowBackArrow { get => navigateBackButton.Visibility == Visibility.Visible; set => navigateBackButton.Visibility = value ? Visibility.Visible : Visibility.Collapsed; }
+
+    public static MainWindow CurrentInstance = null;
+
+    public static DispatcherQueue UIDispatcherQueue = null;
+    public static XamlRoot XamlRoot = null;
+    public static LocalizationHelper localizationHelper = new LocalizationHelper();
+
+    public readonly RestoreWindowManager restoreWindowManager;
+    public readonly WindowStateManager windowStateManager;
+
+
+    public MainWindow()
     {
-        public InactivityHelper inactivityHelper = new InactivityHelper();
-        public static StackPanel InfoMessagesPanel;
-        public Frame MainFrame => navigationFrame;
-        public bool ShowBackArrow { get => navigateBackButton.Visibility == Visibility.Visible; set => navigateBackButton.Visibility = value ? Visibility.Visible : Visibility.Collapsed; }
+        this.InitializeComponent();
 
-        public static MainWindow CurrentInstance = null;
+        CurrentInstance = this;
+        UIDispatcherQueue = DispatcherQueue.GetForCurrentThread();
 
-        public static DispatcherQueue UIDispatcherQueue = null;
-        public static XamlRoot XamlRoot = null;
-        public static LocalizationHelper localizationHelper = new LocalizationHelper();
+        windowStateManager = new WindowStateManager(this);
+        restoreWindowManager = new RestoreWindowManager(this, windowStateManager);
 
-        public MainWindow()
+        restoreWindowManager.RestoreSettings();
+
+        localizationHelper.Initialize();
+
+        Title = Package.Current.DisplayName;
+        this.AppWindow.SetIcon(Path.Combine(Package.Current.InstalledLocation.Path, "Assets\\AppIcon\\appicon.ico"));
+
+        ExtensionHelper.Init();
+        inactivityHelper.InactivityStarted += InactivityHelper_InactivityStarted;
+
+        PasswordHelper.Init();
+
+        InfoMessagesPanel = infoMessagesPanel;
+        ExtendsContentIntoTitleBar = true;
+        SetTitleBar(titleBar);
+        ShowBackArrow = false;
+    }
+
+    private void InactivityHelper_InactivityStarted()
+    {
+        if (this.navigationFrame.CurrentSourcePageType != typeof(LoginPage) &&
+            this.navigationFrame.CurrentSourcePageType != typeof(RegisterPage))
         {
-            this.InitializeComponent();
-
-            CurrentInstance = this;
-            UIDispatcherQueue = DispatcherQueue.GetForCurrentThread();
-
-            localizationHelper.Initialize();
-
-            Title = Package.Current.DisplayName;
-            this.AppWindow.SetIcon(Path.Combine(Package.Current.InstalledLocation.Path, "Assets\\AppIcon\\appicon.ico"));
-
-            ExtensionHelper.Init();
-            inactivityHelper.InactivityStarted += InactivityHelper_InactivityStarted;
-
-            PasswordHelper.Init();
-
-            InfoMessagesPanel = infoMessagesPanel;
-            ExtendsContentIntoTitleBar = true;
-            SetTitleBar(titleBar);
-            ShowBackArrow = false;
-            RestoreSettings();
-
-            this.AppWindow.Closing += AppWindow_Closing;
+            //do not trigger auto logout, when there is an important dialog open e.g. edit or add item dialog
+            if (!AutoLogoutContentDialog.InactivityStarted())
+                return;
+            
+            LogoutHelper.Logout();
+            InfoMessages.AutomaticallyLoggedOut();
+            Database.LoadedInstance.Dispose();
         }
+    }
 
-        private void RestoreSettings()
-        {
-            WindowStateHelper.SetWindowState(this, AppSettings.WindowState);
+    private void NavigateBack_Click(object sender, RoutedEventArgs e)
+    {
+        navigationFrame.GoBack();
+    }
 
-            var width = AppSettings.WindowWidth; ;
-            var height = AppSettings.WindowHeight; 
-            var left = AppSettings.WindowLeft;
-            var top = AppSettings.WindowTop;
-
-            //when closing the window from a minimized state the size will be wrong:
-            if (width < 200)
-                width = 1100;
-            if (height < 100)
-                height = 700;
-
-            if (left != -5000 || top != -5000)
-            {
-                var windowSize = new Windows.Graphics.RectInt32(left, top, width, height);
-                this.AppWindow.MoveAndResize(windowSize);
-            }
-            else
-            {
-                var windowSize = new Windows.Graphics.SizeInt32(width, height);
-                this.AppWindow.Resize(windowSize);
-            }
-        }
-        private void InactivityHelper_InactivityStarted()
-        {
-            if (this.navigationFrame.CurrentSourcePageType != typeof(LoginPage) &&
-                this.navigationFrame.CurrentSourcePageType != typeof(RegisterPage))
-            {
-                //do not trigger auto logout, when there is an important dialog open e.g. edit or add item dialog
-                if (!AutoLogoutContentDialog.InactivityStarted())
-                    return;
-                
-                LogoutHelper.Logout();
-                InfoMessages.AutomaticallyLoggedOut();
-                Database.LoadedInstance.Dispose();
-            }
-        }
-
-        private void AppWindow_Closing(Microsoft.UI.Windowing.AppWindow sender, Microsoft.UI.Windowing.AppWindowClosingEventArgs args)
-        {
-            //save settings:
-            AppSettings.WindowWidth = this.AppWindow.Size.Width;
-            AppSettings.WindowHeight = this.AppWindow.Size.Height;
-            AppSettings.WindowLeft = this.AppWindow.Position.X;
-            AppSettings.WindowTop = this.AppWindow.Position.Y;
-            AppSettings.WindowState = WindowStateHelper.GetWindowState(this);
-        }
-
-        private void NavigateBack_Click(object sender, RoutedEventArgs e)
-        {
-            navigationFrame.GoBack();
-        }
-
-        private void Window_Activated(object sender, WindowActivatedEventArgs args)
-        {
-            if (args.WindowActivationState == WindowActivationState.Deactivated)
-                inactivityHelper.WindowDeactivated();
-            else
-                inactivityHelper.WindowActivated();
-        }
+    private void Window_Activated(object sender, WindowActivatedEventArgs args)
+    {
+        if (args.WindowActivationState == WindowActivationState.Deactivated)
+            inactivityHelper.WindowDeactivated();
+        else
+            inactivityHelper.WindowActivated();
     }
 }
