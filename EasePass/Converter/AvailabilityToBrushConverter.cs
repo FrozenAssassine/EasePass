7fae94ed7568af90705ac9d87a83d1b53aff20a1
Julius Kirsch <Julius@skirsch.de>
2026-01-27 23:28:02 +0100

    fix: get app theme and set to login page combobox color

--- EasePass/Converter/AvailabilityToBrushConverter.cs
+++ EasePass/Converter/AvailabilityToBrushConverter.cs
@@ -1,39 +1,54 @@
-﻿using Microsoft.UI.Xaml.Data;
+﻿using Microsoft.UI;
+using Microsoft.UI.Xaml;
+using Microsoft.UI.Xaml.Data;
 using Microsoft.UI.Xaml.Media;
 using System;
 using System.Collections.Generic;
 using System.Text;
+using Windows.UI;
+using DatabaseAvailability = EasePassExtensibility.IDatabaseSource.DatabaseAvailability;
 
-namespace EasePass.Converter
+namespace EasePass.Converter;
+
+internal sealed class AvailabilityToBrushConverter : IValueConverter
 {
-    internal class AvailabilityToBrushConverter : IValueConverter
+    private static readonly Color LightAvailableColor = Color.FromArgb(255, 0, 0, 0);
+    private static readonly Color LightUnknownColor = Color.FromArgb(255, 255, 200, 0);
+    private static readonly Color LightErrorColor = Color.FromArgb(255, 255, 0, 0);
+
+    private static readonly Color DarkAvailableColor = Color.FromArgb(255, 255, 255, 255);
+    private static readonly Color DarkUnknownColor = Color.FromArgb(255, 255, 255, 0);
+    private static readonly Color DarkErrorColor = Color.FromArgb(255, 255, 80, 80);
+
+    public object Convert(object value, Type targetType, object parameter, string language)
     {
-        public object Convert(object value, Type targetType, object parameter, string language)
-        {
-            if (value == null)
-                return null;
-
-            if (value is not EasePassExtensibility.IDatabaseSource.DatabaseAvailability availability)
-                return null;
-
-            switch (availability)
-            {
-                case EasePassExtensibility.IDatabaseSource.DatabaseAvailability.Available:
-                    return new SolidColorBrush(Windows.UI.Color.FromArgb(255, 255, 255, 255));
-                case EasePassExtensibility.IDatabaseSource.DatabaseAvailability.UnknownState:
-                    return new SolidColorBrush(Windows.UI.Color.FromArgb(255, 255, 255, 0));
-                case EasePassExtensibility.IDatabaseSource.DatabaseAvailability.LockedByOtherUser:
-                    return new SolidColorBrush(Windows.UI.Color.FromArgb(255, 255, 0, 0));
-                case EasePassExtensibility.IDatabaseSource.DatabaseAvailability.Unavailable:
-                    return new SolidColorBrush(Windows.UI.Color.FromArgb(255, 255, 0, 0));
-            }
-
+        if (value is not DatabaseAvailability availability)
             return null;
-        }
 
-        public object ConvertBack(object value, Type targetType, object parameter, string language)
+        return new SolidColorBrush(GetColor(availability, Application.Current.RequestedTheme));
+    }
+
+    private static Color GetColor(DatabaseAvailability availability, ApplicationTheme theme)
+    {
+        bool isDark = theme == ApplicationTheme.Dark;
+
+        return availability switch
         {
-            throw new NotImplementedException();
-        }
+            DatabaseAvailability.Available
+                => isDark ? DarkAvailableColor : LightAvailableColor,
+
+            DatabaseAvailability.UnknownState
+                => isDark ? DarkUnknownColor : LightUnknownColor,
+
+            DatabaseAvailability.LockedByOtherUser
+            or DatabaseAvailability.Unavailable
+                => isDark ? DarkErrorColor : LightErrorColor,
+
+            _ => Colors.Transparent
+        };
+    }
+    public object ConvertBack(object value, Type targetType, object parameter, string language)
+    {
+        throw new NotImplementedException();
     }
 }
